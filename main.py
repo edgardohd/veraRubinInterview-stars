bc557d179d682bda1d487bfa12f4be83a88412b6
Edgardo Huaracan <edgardo.huaracan@siemens.com>
2022-04-07 13:41:51 -0400

    adding getStarsFromrRepo function

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ def main():
     repos = ['freeCodeCamp/freeCodeCamp', '996icu/996.ICU', 'EbookFoundation/free-programming-books']
     # copied from https://www.thepythoncode.com/article/using-github-api-in-python
     for repo in repos:
-        url = f"https://api.github.com/repos/{repo}"
-        user_data = requests.get(url).json()
-        stars_count = user_data['stargazers_count']
-        print(repo + ":" + str(stars_count))
+        getStarsFromRepo(repo)
 
-
+def getStarsFromRepo(repo):
+    url = f"https://api.github.com/repos/{repo}"
+    user_data = requests.get(url).json()
+    stars_count = user_data['stargazers_count']
+    print(repo + ":" + str(stars_count))
+    return stars_count
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
