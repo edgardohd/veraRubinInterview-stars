df1cbe001d36bcfb35ef06a0593cafbe9417bbf5
Edgardo Huaracan <edgardo.huaracan@siemens.com>
2022-04-07 13:42:13 -0400

    adding mainSpec with one unit test

---
+++ mainSpec.py
@@ -0,0 +1,8 @@
+import main
+
+# from https://requests-mock.readthedocs.io/en/latest/pytest.html
+def test(requests_mock):
+    repo = "user/repoName"
+    stars = 123456
+    requests_mock.get(f"https://api.github.com/repos/{repo}", json={'stargazers_count': stars, 'anyOtherKey': 'anyOtherValue'})
+    assert main.getStarsFromRepo("user/repoName") == stars
