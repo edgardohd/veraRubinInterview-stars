ad83c5ea9e55884265c168ccf9b811f9b64dbe05
Edgardo Huaracan <edgardo.huaracan@siemens.com>
2022-04-07 14:16:32 -0400

    return YAML output

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#Edgardo Huaracan 2022
 import requests
-
-
-
+import yaml
 
 def main():
+    dict = {}
     repos = ['freeCodeCamp/freeCodeCamp', '996icu/996.ICU', 'EbookFoundation/free-programming-books']
     # copied from https://www.thepythoncode.com/article/using-github-api-in-python
     for repo in repos:
-        getStarsFromRepo(repo)
+        dict[repo] = getStarsFromRepo(repo)
+    output = yaml.dump(dict, default_flow_style=False)
+    print(output)
+    return yaml.dump(output)
 
 def getStarsFromRepo(repo):
     url = f"https://api.github.com/repos/{repo}"
     user_data = requests.get(url).json()
     stars_count = user_data['stargazers_count']
-    print(repo + ":" + str(stars_count))
+    #print(repo + ":" + str(stars_count))
     return stars_count
 
-# Press the green button in the gutter to run the script.
+
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
