5795230f5e1b7f1c70af8829099316e9f0dbe300
Edgardo Huaracan <edgardo.huaracan@siemens.com>
2022-04-07 12:55:32 -0400

    print star count from a list of repo

--- main.py
+++ main.py
@@ -3,19 +3,18 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import requests
-from pprint import pprint
 
 
 
 
 def main():
-
-    username = "EbookFoundation"
-    repo = "free-programming-books"
-    url = f"https://api.github.com/repos/{username}/{repo}"
-    user_data = requests.get(url).json()
-    stars_count = user_data['stargazers_count']
-    print(stars_count)
+    repos = ['freeCodeCamp/freeCodeCamp', '996icu/996.ICU', 'EbookFoundation/free-programming-books']
+    # copied from https://www.thepythoncode.com/article/using-github-api-in-python
+    for repo in repos:
+        url = f"https://api.github.com/repos/{repo}"
+        user_data = requests.get(url).json()
+        stars_count = user_data['stargazers_count']
+        print(repo + ":" + str(stars_count))
 
 
 
